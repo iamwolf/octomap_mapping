1dfbb6f952914d3a2c2eba7285684649bbc4ef20
Wolfgang Merkt <w.merkt@gmail.com>
2015-10-29 16:02:17 +0000

    Linting TrackingOctomapServer.h

--- octomap_server/include/octomap_server/TrackingOctomapServer.h
+++ octomap_server/include/octomap_server/TrackingOctomapServer.h
@@ -30,6 +30,7 @@
 #ifndef OCTOMAP_SERVER_TRACKINGOCTOMAPSERVER_H_
 #define OCTOMAP_SERVER_TRACKINGOCTOMAPSERVER_H_
 
+#include <string>
 #include "octomap_server/OctomapServer.h"
 
 namespace octomap_server
@@ -38,7 +39,7 @@ namespace octomap_server
 class TrackingOctomapServer: public OctomapServer
 {
 public:
-  TrackingOctomapServer(const std::string& filename = "");
+  explicit TrackingOctomapServer(const std::string& filename = "");
   virtual ~TrackingOctomapServer();
 
   void trackCallback(sensor_msgs::PointCloud2Ptr cloud);
@@ -57,5 +58,5 @@ protected:
   ros::Subscriber subFreeChanges;
 };
 
-} /* namespace octomap */
-#endif /* TRACKINGOCTOMAPSERVER_H_ */
+}  // namespace octomap_server
+#endif  // OCTOMAP_SERVER_TRACKINGOCTOMAPSERVER_H
